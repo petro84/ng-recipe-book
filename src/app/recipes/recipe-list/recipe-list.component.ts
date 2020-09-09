56facf216d5f390bfd89c56d95b45e053e7ed798
Larry Petroski <larry@Petro.local>
2020-09-09 13:17:39 -0500

    section 3 completed

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'Some description', 'https://p0.pikist.com/photos/666/25/food-meat-recipe-power-pork-dishes.jpg')
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
