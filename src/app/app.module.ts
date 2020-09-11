b280aaeb1ab35682affcf331388e845947862096
Larry Petroski <larry@Petro.local>
2020-09-11 11:11:43 -0500

    section 21 completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,8 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
 import { AuthComponent } from './auth/auth.component';
 import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
 import { AuthInterceptorService } from './auth/auth-interceptor.service';
+import { AlertComponent } from './shared/alert/alert.component';
+import { PlaceholderDirective } from './shared/placeholder/placeholder.directive';
 
 @NgModule({
   declarations: [
@@ -33,7 +35,9 @@ import { AuthInterceptorService } from './auth/auth-interceptor.service';
     RecipeHomeComponent,
     RecipeEditComponent,
     AuthComponent,
-    LoadingSpinnerComponent
+    LoadingSpinnerComponent,
+    AlertComponent,
+    PlaceholderDirective
   ],
   imports: [
     BrowserModule,
--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,24 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { Observable } from 'rxjs';
-import { Router } from '@angular/router';
+import {
+  Component,
+  ComponentFactoryResolver,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { Observable, Subscription } from "rxjs";
+import { Router } from "@angular/router";
 
-import { AuthService, AuthResponseData } from './auth.service';
+import { AuthService, AuthResponseData } from "./auth.service";
+import { PlaceholderDirective } from "../shared/placeholder/placeholder.directive";
+import { AlertComponent } from "../shared/alert/alert.component";
 
 @Component({
-  selector: 'app-auth',
-  templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.css']
+  selector: "app-auth",
+  templateUrl: "./auth.component.html",
+  styleUrls: ["./auth.component.css"],
 })
-export class AuthComponent implements OnInit {
+export class AuthComponent implements OnInit, OnDestroy {
   isLoginMode = true;
   isLoading = false;
-  error: string = null;
+  private alertSub: Subscription;
 
-  constructor(private aService: AuthService, private router: Router) { }
+  @ViewChild(PlaceholderDirective) alertHost: PlaceholderDirective;
 
-  ngOnInit(): void {
-  }
+  constructor(
+    private aService: AuthService,
+    private router: Router,
+    private cfr: ComponentFactoryResolver
+  ) {}
+
+  ngOnInit(): void {}
 
   onSwitchMode() {
     this.isLoginMode = !this.isLoginMode;
@@ -31,23 +44,46 @@ export class AuthComponent implements OnInit {
 
     const email = form.value.email;
     const password = form.value.password;
-    let authObs: Observable<AuthResponseData>
+    let authObs: Observable<AuthResponseData>;
 
     this.isLoading = true;
     if (this.isLoginMode) {
-      authObs = this.aService.login(email, password)
+      authObs = this.aService.login(email, password);
     } else {
       authObs = this.aService.singup(email, password);
     }
 
-    authObs.subscribe((respData) => {
-      this.isLoading = false;
-      this.router.navigate(['/recipes']);
-    }, err => {
-      this.error = err;
-      this.isLoading = false;
-    });
+    authObs.subscribe(
+      (respData) => {
+        this.isLoading = false;
+        this.router.navigate(["/recipes"]);
+      },
+      (err) => {
+        this.showErrorAlert(err);
+        this.isLoading = false;
+      }
+    );
 
     form.reset();
+  }
+
+  private showErrorAlert(message: string) {
+    const alertCmpFactory = this.cfr.resolveComponentFactory(AlertComponent);
+
+    const hostViewContainerRef = this.alertHost.viewContainerRef;
+    hostViewContainerRef.clear();
+
+    const componentRef = hostViewContainerRef.createComponent(alertCmpFactory);
+    componentRef.instance.message = message;
+    this.alertSub = componentRef.instance.close.subscribe(() => {
+      this.alertSub.unsubscribe();
+      hostViewContainerRef.clear();
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.alertSub) {
+      this.alertSub.unsubscribe();
+    }
   }
 }
